722e4ac6095017bb199fe3c902dcf3d74edb3cec
parth <parthsrivastava100@gmail.com>
2020-08-07 15:18:30 +0530

    results added

--- bleurt_test.py
+++ bleurt_test.py
@@ -10,24 +10,24 @@ def scoress(a,b):
     tot.append(scores)
 
 import pandas as pd
-train=pd.read_csv('/home/parth/NLG_evaluation/datasets/SICK_train - SICK_train.csv')
+train=pd.read_csv('/home/parth/NLG_evaluation/datasets/SICK_test_annotated - SICK_test_annotated.csv')
 ref_list=[]
-for i in range(4500):
+for i in range(4927):
   if (train['sentence_A'][i]).endswith('.'):
     ref_list.append((train['sentence_A'][i]))
   else :
     ref_list.append((train['sentence_A'][i]+'.'))
 hyp_list=[]
-for i in range(4500):
+for i in range(4927):
   if (train['sentence_B'][i]).endswith('.'):
     hyp_list.append((train['sentence_B'][i]))
   else :
     hyp_list.append((train['sentence_B'][i]+'.'))
-for i in range(4500):
+for i in range(4927):
   scoress(ref_list[i],hyp_list[i])
 anno=(train['relatedness_score'])
 anno=anno/5
-f=open('results.txt','w')
+f=open('results_test.txt','w')
 f.write(tot)
 f.write('   ')
 f.write(anno)
